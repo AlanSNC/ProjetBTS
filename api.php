5fe951afe7e2d2bd07f7fd5ba218e051ed616821
AlanSNC <asenac@condorcet93.fr>
2022-03-08 09:43:43 +0100

    GetPlaqueByName();

--- api.php
+++ api.php
@@ -20,44 +20,24 @@ if($con){
 else{
     echo "DataBase connection failed";}
     
-   /* function updatePlaque($Nom)
- {
- global $conn;
- $data = json_decode(file_get_contents("php://input"),true);
- $Plaque = $data["Plaque"];
- $query="UPDATE plaque_allow SET Plaque='".$Plaque."' WHERE Nom=".$Nom;
- if(mysqli_query($conn, $query))
- {
- $response=array(
- 'status' => 1,
- 'status_message' =>'plaque_allow updated with success.'
- );
- }
- else
- {
- $response=array(
- 'status' => 0,
- 'status_message' =>'Failure with the plaque_allow update'.
-mysqli_error($conn)
-);}
- }
 
-updatePlaque(Test);*/
 
-    function getPlaque()
- {
- global $conn;
- $query = "SELECT * FROM plaque_allow";
- $response = array();
- $result = mysqli_query($conn, $query);
- while($row = mysqli_fetch_array($result))
- {
- $response[] = $row;
- }
- header('Content-Type: application/json');
- echo json_encode($response, JSON_PRETTY_PRINT);
+    function getPlaqueByName($Nom=0)
+{
+global $conn;
+$query = "SELECT * FROM Plaque_allow";
+if($ID != 0){
+    $query .= " WHERE Nom=".$Nom." LIMIT 1";
+}
+$response = array();
+$result = mysqli_query($conn, $query);
+while($row = mysqli_fetch_array($result)){
+    $response[] = $row;
+}
+header('Content-Type: application/json');
  }
 
  getPlaque();
+ getPlaqueByName(Test);
 
     ?>
